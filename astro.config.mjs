2ce13e69403f2959102b5a78b057d12a3b30255a
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2025-06-20 14:03:24 -0700

    camiada nuevamente la ruta de astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://Adrinc.github.io',
   integrations: [tailwind(), react()],
   output: 'server',
   adapter: node({
